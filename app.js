0c038a3c83981756f67190965252592e3a9f458e
arya <arya.spillai@magpiitech.com>
2026-08-31 14:49:07 +0530

    db

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const express = require("express"); 
-const app = express(); 
-const dbPassword = "MySecretPassword123";
-app.get("/", function(req, res) { 
-res.send("Hello DevSecOps"); 
-}); 
+const express = require("express");
+const app = express();
+
+app.get("/user", function(req, res) {
+
+    const user = req.query.user;
+
+    const query = "SELECT * FROM users WHERE username = '" + user + "'";
+
+    console.log(query);
+
+    res.send("User: " + user);
+});
+
 app.listen(3000);
